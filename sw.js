b4a505a2e3e0881ac1c0853d3133864389e82f08
Stitch <stitch78140@gmail.com>
2025-09-15 14:57:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — met le site en cache pour hors-ligne
-const CACHE_VERSION = 'v1.0.0';
+const CACHE_VERSION = 'v1.0.1';
 const CACHE_NAME = `caisse-${CACHE_VERSION}`;
 const ASSETS = [
   './',
